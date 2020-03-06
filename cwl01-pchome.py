f6d2d09c4b01b287ed3205d4b2adc7d0f5858661
eec22 <eec22977954@gmail.com>
2020-03-06 13:30:32 +0800

    基本爬蟲

--- cwl01-pchome.py
+++ cwl01-pchome.py
@@ -12,11 +12,11 @@ for i in range(1, 30):
     prods = data['prods']
     # print(prods)
     for news in prods:
-        id = news['Id']
+        keyid = news['Id']
         name = news['name']
         price = news['price']
-        cur.execute("insert into prod_vga(id, pname, date, price) values(?,?,DATE('now','localtime'),?)",
-                    (id, name, price))
+        cur.execute("insert into prod_vga(keyid, pname, regdate, price) values(?,?,DATE('now','localtime'),?)",
+                    (keyid, name, price))
         #建立一個時間
         conn.commit()
         #在這邊提交是為了防止連續連線讓系統產生訊息，訊息會造成辨識失敗，這邊先提交可以避免寫入沒有完成
@@ -24,6 +24,7 @@ for i in range(1, 30):
     # 刪除其他非目標分類
     # 有機會刪到不應該刪的
     # 資料庫格式還有待修改
+
 #有機會出現JSON查詢錯誤失敗之類的狀況，但是是因為出現"系統忙碌中，請稍後在試..."的錯誤訊息，所以被判定錯誤，
 #但是在迴圈內就Commit的話就算出現錯誤資料也已經寫入，出現錯誤無妨
 conn.commit()
